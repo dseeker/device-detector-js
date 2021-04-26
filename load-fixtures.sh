65e5d4de58cd6f58054585e10e470e79b7ba01e0
DSeeKer <dseeker@users.noreply.github.com>
2021-04-26 21:49:29 +0200

    deploy in github

--- load-fixtures.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-napa
-rm -rf fixtures
-node load-fixtures.js
-rm -rf node_modules/device-detector
--- src/utils/trim.ts
+++
@@ -1,5 +0,0 @@
-export const trim = (str: string, char: string) => {
-  return str.replace(new RegExp(
-    "^[" + char + "]+|[" + char + "]+$", "g"
-  ), "");
-};
